5c7e1ce3a680053a5c6d376c45b8dc9bbaa89a3e
bluayer <ijacsong98@gmail.com>
2018-03-24 16:29:19 +0900

    3/24 node-study
    npm init 후 express 설치
    Express server를 시작함으로써 서버로부터 받은 응답을 3000번 포트를 통해 띄우는 것을 확인했다.

---
+++ 3_24_node02.js
@@ -0,0 +1,20 @@
+var express = require('express'), http = require('http');
+
+// 익스프레스 객체 생성
+var app = express();
+
+// 기본 포트를 app 객체에 속성으로 설정
+app.set('port', process.env.PORT || 3000);
+
+// 기본 포트를 app 객체에 속성으로 설정
+app.use(function(req, res, next) {
+	console.log('첫 번째 미들웨어에서 요청을 처리함');
+
+	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+	res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+});
+
+// Express 서버 시작
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('익스프레스 서버를 시작헀습니다 : ' + app.get('port'))
+});
