2be50cfe0c3c2a52ca12e1997d53171f01e1d515
bluayer <ijacsong98@gmail.com>
2018-03-26 10:50:39 +0900

    3/26 01 study about middleware

--- 3_26_node01.js
+++ 3_26_node01.js
@@ -6,12 +6,21 @@ var app = express();
 // 기본 포트를 app 객체에 속성으로 설정
 app.set('port', process.env.PORT || 3000);
 
-// 기본 포트를 app 객체에 속성으로 설정
+// 첫 번째 미들웨어에서 특정 객체 설정
 app.use(function(req, res, next) {
 	console.log('첫 번째 미들웨어에서 요청을 처리함');
 
+	req.user = '정우';
+
+	next();
+});
+
+// 두 번째 미들웨어에서 핸들링
+app.use(function(req, res, next) {
+	console.log('두 번째 미들웨어에서 요청을 처리함');
+
 	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-	res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+	res.end('<h1>Express 서버에서 ' + req.user +'가 응답한 결과입니다.</h1>');
 });
 
 // Express 서버 시작헀습니다
